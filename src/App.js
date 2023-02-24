4fc70eaee86f72f76ddac807fb06240ebe2ab22b
rybkinIt <116676756+rybkinIt@users.noreply.github.com>
2023-02-24 17:21:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className="p1"></div>
+      <div className="p2"></div>
+      <div className="p3"></div>
     </div>
   );
 }
